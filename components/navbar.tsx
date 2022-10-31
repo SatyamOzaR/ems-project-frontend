a163760b58dd5fc6f5d8d4293f523d34f8e554d7
SatyamOzaR <satyamozar@gmail.com>
2022-10-31 20:53:19 +0530

    getStaticProps

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,10 +13,7 @@ export default function NavBar() {
       <img src="/cbrex_icon.png" className="mr-3 h-6 sm:h-9" alt="CBREX Logo"/>
       <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Employee Management System</span>
   </a>
-  <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
-    <span className="sr-only">Open main menu</span>
-    <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-  </button>
+  
   <div className="hidden w-full md:block md:w-auto" id="navbar-default">
     <ul className="flex flex-col p-4 mt-4 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-yellow-500">
       <li>
--- pages/all_employees.tsx
+++ pages/all_employees.tsx
@@ -2,16 +2,53 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Header />
-      
-      <main className="hero container max-w-screen-lg mx-auto pb-10 mt-5">
-      <h1>all_employees</h1>
-      </main>
-      
-      <Footer />
-    </div>
-  )
+export const getStaticProps = async () => {
+  const res = await fetch('http://localhost:8080/api/employees');
+  const data = await res.json()
+  return {
+      props: {
+          data: data
+      }
+  };
 }
+
+const AllEmployees = ({data} : any) => 
+<>
+  <div className={styles.container}>
+      <Header />
+      <div className="overflow-x-auto relative">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" className="py-3 px-6">
+                    First Name
+                </th>
+                <th scope="col" className="py-3 px-6">
+                    Last Name
+                </th>
+                <th scope="col" className="py-3 px-6">
+                    Email 
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        {data.map((x: { firstName: string; lastName: string; email: string; }, i: any) =>
+            <tr key={i} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {x.firstName}
+                </th>
+                <td className="py-4 px-6">
+                {x.lastName}
+                </td>
+                <td className="py-4 px-6">
+                {x.email}
+                </td>
+            </tr>)}
+        </tbody>
+    </table>
+      </div>
+      <Footer />
+  </div>
+</>
+
+export default AllEmployees
